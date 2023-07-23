f261f6bc193c13f5f00f2923f03f2369b17b82f2
itaysh24-meet <yatishav100@gmail.com>
2023-07-23 16:22:07 +0300

    done

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -1,20 +1,59 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
-import pyrebase
+import pyrebase 
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
+const firebaseConfig = {
+
+  apiKey: "AIzaSyArWxllAd3iJglgNz_RYQK-z1nSzmepIDY",
+
+  authDomain: "intrepid-fiber-328519.firebaseapp.com",
+
+  projectId: "intrepid-fiber-328519",
+
+  storageBucket: "intrepid-fiber-328519.appspot.com",
+
+  messagingSenderId: "978423849068",
+
+  appId: "1:978423849068:web:3eada0ef0c19cf86763849",
+
+  measurementId: "G-M1SX87MV99"
+
+};
+
+const app = pyrebase.initializeApp(firebaseConfig);
+const analytics = pyrebase.getAnalytics(app);
 
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
-    return render_template("signin.html")
+    if request.method = "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        try:
+            login_session["loged_user"] = [email, password]
+            return redirect(url_for("/add_tweet"))
+        except:
+            return "login failed"
+    else:
+        return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    if request.method = "POST":
+        email = request.form["email"]
+        password = request.form["password"]
 
+        try:
+            login_session["user"] = [email, password]
+            return redirect(url_for("/add_tweet"))
+        except:
+            return "authorize failed"
+    else:
+        return render_template("signup.html")
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
